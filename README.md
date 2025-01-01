681466f951fcda80b02ab77afaa2825fb0d925e6
ZoloniChase <zolonichase153@gmail.com>
2025-01-01 18:21:37 -0400

    Version2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-This website/application is supposed to show all the bus routes in Georgetown. This was made for me to practice my coding skills. A video demonstration can be seen in the video.txt file.
+This website is supposed to show all the bus routes in Georgetown. This was made for me to practice my coding skills. A video demonstration of the various drafts/versions can be seen in the video.txt file.
+
+Technologies used: Python Flask, HTML,CSS,JAVASCRIPT,SQL, Google Maps API and Google Directions API
--- static/app.js
+++ static/app.js
@@ -1,55 +1,48 @@
-function initMap(x,y) { //given 2 params so that we can call location and location1 later
-    //const location = { lat: 6.8046, lng: -58.1553 };
-    //const location1 = { lat: 6.808014, lng: -58.162764 };
-    const center = { lat: 6.8046, lng: -58.1553 } //GT coordinates
+function initMap() {
+    const center = { lat: 6.8046, lng: -58.1553 }; // GT coordinates
 
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 12,
         center: center,
     });
-    
-    new google.maps.Marker({
-        position: x, //param for marker
-        map: map,
-    });
 
-    new google.maps.Marker({
-        position: y, //param for marker
-        map: map,
-    });
-    
+    const directionService = new google.maps.DirectionsService(); // Calculate route between locations
+    const renderDirections = new google.maps.DirectionsRenderer(); // Display route on map
+    renderDirections.setMap(map);
 
-    var destination = [x, y];
-    var route = new google.maps.Polyline({
-        path: destination,
-        strokeColor: "#0000FF",
-        strokeOpacity: 0.8,
-        strokeWeight: 5
+    function directions(x, y) {
+        const waypoint = {
+            origin: x,
+            destination: y,
+            travelMode: 'DRIVING'
+        }; // Required parameters given by Google Directions API website
 
-    })
-    route.setMap(map) 
+        directionService.route(waypoint, (response, status) => {
+            if (status === "OK") {
+                renderDirections.setDirections(response);
+            } else {
+                console.error("No direction", status);
+            }
+        });
+    }
 
+    function plot() {
+        const form = document.getElementById("bus-form"); // Get information from bus-form id in HTML
+        form.addEventListener("submit", (e) => {
+            e.preventDefault(); // Prevent default form submission
+            const bus_num = document.querySelector('input[name="bus_num"]').value; // Ensure your form has an id like 'bus-form'
 
+            fetch(`bus_info?bus_num=${bus_num}`) // Get the bus_num and store it in variable
+                .then(response => response.json())
+                .then(data => { // JSON info will be stored in data object
+                    const location = { lat: data.start_lat, lng: data.start_long };
+                    const location1 = { lat: data.stop_lat, lng: data.stop_long };
 
+                    directions(location, location1);
+                });
+        });
+    }
+
+    // Call plot function after DOM loads
+    plot()
 }
-
-function plot(){
-    const form = document.getElementById("bus-form"); // get information from bus-form id in html
-    form.addEventListener("submit", (e) => {
-        e.preventDefault(); // Prevent default form submission
-        const bus_num = document.querySelector('input[name="bus_num"]').value;// Ensure your form has an id like 'bus-form'
-    
-
-    fetch(`bus_info?bus_num=${bus_num}`) //get the bus_num and store it in variable
-    .then(response => response.json())
-    .then(data=>{ //json info will be stored in data object
-    const location = {lat:data.start_lat, lng:data.start_long};
-    const location1 = { lat: data.stop_lat, lng: data.stop_long }; 
-
-    initMap(location,location1)
-    }) 
-
-})
-}
-// Call plot function after DOM loads
-document.addEventListener("DOMContentLoaded", plot);
