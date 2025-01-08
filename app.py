6ce3266a2feba53c5819056172b35a9bdd60139d
ZoloniChase <zolonichase153@gmail.com>
2025-01-07 22:06:26 -0400

    Minor changes

--- app.py
+++ app.py
@@ -21,17 +21,14 @@ def test_db():
 @app.route("/bus_info", methods=['GET'])
 def query():
     try:
-        bus_num = request.args.get('bus_num') #request bus_num from html name attribute in the input field
-        
-        
         conn = get_db_connection()
         cursor = conn.cursor(dictionary=True) 
         
-        sql = "SELECT start_lat, start_long, stop_lat, stop_long FROM bus_info WHERE bus_num = %s" #%s is a placeholder string depending on the input
+        sql = "SELECT start_lat, start_long, stop_lat, stop_long FROM bus_info" #%s is a placeholder string depending on the input
         #sql stmt
         
-        cursor.execute(sql, (bus_num,)) #executes the sql stmt and gets the input fron bus_num
-        result = cursor.fetchone() #fetch one column
+        cursor.execute(sql) #executes the sql statement
+        result = cursor.fetchall() #fetch all columns
         
         if result: # if the number is there return the coordinates in json format
             return jsonify(result)
